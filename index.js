b06723226539aa64f05893192e44dc4b38c034d1
李峰 <1783492844@qq.com>
2020-07-06 10:21:12 +0800

    理清逻辑，修改代码

--- index.js
+++ index.js
@@ -1,131 +1,58 @@
-var left = "0";
-var right = "";
-var operator = null;
-// 当前输入的数是属于运算符左侧还是右侧【0】左侧；【1】右侧
-var index = 0;
+var screen,
+	// 用于没有输入时显示0，有显示时清空
+	flag = true,
+	// 记录最后一次输入的运算符
+	operator,
+	// 记录最后一次输入的数
+	lastNum,
+	// 连等次数
+	equalNum = 0
 
+function inputNumber(nums) {
+	// 输入数字或运算符时重置连等次数
+	equalNum = 0
+	screen = document.getElementById("screen")
+	// 清除没有输入时的0
+	if (flag) {
+		screen.value = ""
+	}
 
-// 清屏
+	// 改变状态，不再清空内容
+	flag = false
+
+	// 将之前的内容与输入的内容相加，重新显示
+	screen.value += nums
+
+	// 记录最新一次输入的运算符，为连等运算做准备
+	if (nums == "+" || nums == "-" || nums == "/" || nums == "*") {
+		operator = nums
+	}
+}
+
+// 暂存最后一次的运算符
+function cacheOperator() {
+
+}
+
 function clear1() {
-	left = "0";
-	right = "";
-	operator = "";
-	index = 0;
-	document.getElementById("screen").value = "0";
+	screen = document.getElementById("screen")
+	screen.value = "0"
+	// 让下次输入可以清空0
+	flag = true
 }
 
-
-
-// 输入小数点
-function inputDecimalPoint() {
-	clear1();
-
-	// todo:小数点是否可再次输入，以及第一次输入小数点
-
-	input(".");
-}
-
-// 输入数字
-function inputNumber(num) {
-	let screen = document.getElementById("screen");
-
-	// 输入左侧数据
-	if (index == 0) {
-		// 如果输入左侧数据时，右侧数据不为空，则表明这次输入为新的输入，需要清除界面
-		if (right != "") {
-			clear1();
-		}
-		// 如果左侧数据为0
-		if (left == "0") {
-			// 如果当前输入的数字不为0，则更新左侧数据为当前输入的数字
-			if (num != 0) {
-				left = num;
-			}
-
-			screen.value = left;
-			return;
-		}
-
-		// 如果左侧数据不为0
-		left = left + "" + num;
-		screen.value = left;
-		return;
+function equal() {
+	equalNum++
+	screen = document.getElementById("screen")
+	// 如果连等次数为1，则记录最后一次输入的数，为连等运算做准备
+	if (equalNum == 1) {
+		let arr = screen.value.split(operator)
+		lastNum = arr[arr.length - 1]
 	}
-	// 输入右侧数据
-	right = right + "" + num;
-	screen.value = left + operator + right;
-}
-
-// 输入运算符
-function inputOperator(inputOpe) {
-
-
-	// 如果输入右侧数据点击运算符
-	if (index == 1) {
-		// 如果右侧数据不为空，即右侧数据输入完成，则计算结果并再次加上这次的运算符
-		if (right != "") {
-			equal(operator);
-			return;
-		}
-
-		
-		// todo:乘以负数
-		right += inputOpe;
-
+	// 如果连等次数超过1，则进行连等运算
+	if (equalNum > 1) {
+		screen.value = eval(screen.value + operator + lastNum)
+		return
 	}
-	operator = inputOpe;
-
-	document.getElementById("screen").value = left + inputOpe;
-
-	// 接下来输入右侧数据
-	index = 1;
-	right = "";
-
-}
-
-function input(c) {
-	let screen = document.getElementById("screen");
-	left = screen.value;
-	console.log(left);
-
-	screen.value = left + c;
-}
-
-// 等于，计算结果
-function equal(ope) {
-
-	let screen = document.getElementById("screen");
-	let val = screen.value;
-	let res = '';
-	try {
-		res = eval(val);
-	} catch (e) {
-		res = 'Error';
-	}
-
-	left = res;
-
-	console.log(left, operator, right, index);
-
-	// 如果是点击“=”号，则只需显示结果
-	if (!ope) {
-		// 如果index还未置0，则进行正常置0操作
-		if (index == 1) {
-			screen.value = res;
-			index = 0;
-			console.log(left, operator, right, index);
-			return;
-		}
-
-		// 如果index已置0，且操作符不为空，则进行连续运算操作
-		if (operator != "") {
-			screen.value = eval(left + operator + right);
-			left = screen.value;
-		}
-		return;
-	}
-	// 如果的点击其他运算符，则显示 结果+运算符
-	screen.value = res + ope;
-	right = "";
-	index = 1;
+	screen.value = eval(screen.value)
 }
